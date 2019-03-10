9f04406c9247b1a8cf55a8f93278503ee28d9b28
PCSE Research <pcse.research@gmail.com>
2019-03-10 14:21:54 -0400

    Updates to test script

--- angles_contour_test.py
+++ angles_contour_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+import imutils
 
 im = cv2.imread('SHMS_angle_7562.jpg')
 cv2.imshow("Orig",im)
@@ -13,7 +14,10 @@ ret,thresh = cv2.threshold(imgray,127,255,0)
 cv2.imshow("Thresh",thresh)
 cv2.waitKey(0)
 
-contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+if imutils.is_cv4():
+	contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+else:
+	contours = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 cv2.drawContours(im,contours,-1, (0,255,0), 3)
 cv2.imshow("Contours",im)
