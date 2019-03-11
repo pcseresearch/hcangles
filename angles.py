8cb3fa4e10a1e5e6e79465e9f8f75d6b234871a2
PCSE Research <pcse.research@gmail.com>
2019-03-11 18:48:54 -0400

    Mods to main script to analyze full image

--- angles.py
+++ angles.py
@@ -6,21 +6,28 @@ import imutils
 import cv2
 import os
 import subprocess
+from pylab import *
+from scipy.optimize import curve_fit
+import matplotlib.pyplot as plt
+
+def fitfunction(x,a,b):
+    xp = np.array(x)
+    return a+b*xp
 
 run = input("Run Number:")
 
-extract_image_command = "../spectrometer_angles/bin/extract_coda_images ../spectrometer_angles/raw/coin_all_0%s.dat -r %s" % (run,run)
+extract_image_command = "../spectrometer_angles/bin/extract_code_images ../spectrometer_angles/raw/coin_all_0%s.dat -r %s" % (run,run)
 
-print ("Extract image command = ", extract_image_command)
+print ("Extract image command = ",extract_image_command)
 os.system(extract_image_command)
 
 file = open("SHMS_encoder_" + run + ".dat")
 lang = []
 
 with open("SHMS_encoder_" + run + ".dat") as f:
-	for line in f:
-		data = line.split()
-		lang.append(float(data[0]))
+    for line in f:
+        data = line.split()
+        lang.append(float(data[0]))
 
 file.close()
 
@@ -36,67 +43,119 @@ squrKernel = cv2.getStructuringElement(cv2.MORPH_RECT,(1,1))
 
 
 image = cv2.imread("SHMS_angle_" + run + ".jpg")
-image = imutils.resize(image,width=300)
+#image = imutils.resize(image,width=300)
 gray = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+#cv2.imshow("Gray",gray)
+#cv2.waitKey(0)
 
-tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, rectKernel)
+tophat = cv2.morphologyEx(gray, cv2.MORPH_TOPHAT, sqreKernel)
 
 
-gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0,
-	ksize=-1)
+gradX = cv2.Sobel(tophat, ddepth=cv2.CV_32F, dx=1, dy=0,ksize=-1)
 gradX = np.absolute(gradX)
 (minVal, maxVal) = (np.min(gradX), np.max(gradX))
 gradX = (255 * ((gradX - minVal) / (maxVal - minVal)))
 gradX = gradX.astype("uint8")
 
 
-gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, rectKernel)
+gradX = cv2.morphologyEx(gradX, cv2.MORPH_CLOSE, sqreKernel)
+#cv2.imshow("GradX",gradX)
+#cv2.waitKey(0)
 
 
-thresh = cv2.threshold(gradX, 0, 255,
-	cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+thresh = cv2.threshold(gradX, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+#cv2.imshow("Thresh",thresh)
+#cv2.waitKey(0)
 
 
 thresh = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, sqKernel)
-#cv2.imshow("Image Thresh Again",thresh)
+#cv2.imshow("Thresh Again",thresh)
+#cv2.waitKey(0)
 
 if imutils.is_cv4():
-	cnts,hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts,hierarchy = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 else:
-	cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-	if imutils.is_cv2():
-		cnts = cnts[0]
-	else:
-		cnts = cnts[1]
+    cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if imutils.is_cv2():
+        cnts = cnts[0]
+    else:
+        cnts = cnts[1]
 
-cv2.drawContours(image,cnts,-1, (0,255,0), 3)
+#cv2.drawContours(image,cnts,-1, (0,255,0), 3)
 #cv2.imshow("With Contours",image)
 #cv2.waitKey(0)
 
+middleval = [0.0 for i in range(0,21)]
+count = [0 for i in range(0,21)]
+index = -10
+
 for (i, c) in enumerate(cnts):
-	(x, y, w, h) = cv2.boundingRect(c)
-	ar = w / float(h)
-	if (ar>0.0001):
-		print (i,x,y,w,h,ar)
-	if ar > 5 and y > 100 and y < 165 and x > 75 and x < 210:
-		print("Vernier Scale ROI Values:")
-		print(x,y,w,h,ar)
-		print("Veriner Scale Center Value:")
-		middle = (w/2) + x
-		print(middle)
-	if x > 20 and x < 60 and ar < 1:
-		xvalue = x + (56*2)
-		print("Center Marker Value:")
-		print(xvalue)
+    (x, y, w, h) = cv2.boundingRect(c)
+    ar = w / float(h)
+    #if (ar>0.0001):
+    #	print (i,x, y, w, h, ar)
+    if w > 5 and h > 22 and y > 260 and y < 270 and x > 150 and x < 660:
+        print("Vernier Scale ROI Values:")
+        print(x,y,w,h,ar)
+        print("Vernier Scale Center Value:")
+        if w > 10:
+            print("Vernier Scale Double:")
+            count[index+10] = index
+            count[index+11] = index+1
+            middleval[index+10] = (w/4) + x
+            middleval[index+11] = (3*w/4) + x
+            index = index + 2
+        else:
+            print("Vernier Scale Single:")
+            count[index+10] = index
+            middleval[index+10] = (w/2) + x
+            index = index + 1
+
+middleval.sort()
+
+print (middleval)
+print (count)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(111)
+
+ax1.set_title("Vernier Scale Positions")
+ax1.set_xlabel('Index of Vernier Marker')
+ax1.set_ylabel('X-position (pixels)')
+ax1.set_yscale("linear",nonposy='clip')
+ax1.grid(True)
+ax1.scatter(count,middleval)
+
+init_vals = [400.0,50.0]
+popt, pcov = curve_fit(fitfunction, count, middleval, p0=init_vals)
+
+print (popt[0],popt[1])
+print (pcov)
+
+ax1.plot(count,fitfunction(count, *popt), 'r-', label = "Linear Fit")
+leg = ax1.legend()
+plt.show()
+
+middle = popt[0]
+xmin = middle - 5*popt[1]
+xmax = middle + 5*popt[1]    
+ycentral_max = 0
+
+for (i, c) in enumerate(cnts):
+    (x, y, w, h) = cv2.boundingRect(c)
+    ar = w / float(h)
+    if x > xmin and x < xmax and y > 120 and y < 260 and w > 3 and h > 3 and h > ycentral_max:
+        print("Center Marker Value:")
+        print(x,y,w,h,ar)
+        ycentral_max = h
+        xvalue = (w/2) + x
+        print(xvalue)
 
 print("Hundredth Angle Value:")
-result = (xvalue - middle)/(50)
-sang = (result)/(10)
-central_angle = float(int(10*lang[0])/10.0)
+result = (xvalue - middle)/popt[1]
+sang = (result)/(100)
+central_angle = float(round(10*lang[0])/10.0)
 final = central_angle+sang
 print(sang)
 print("Final Angle Value:")
 print(final)
-
-cv2.waitKey(0)
-exit()
